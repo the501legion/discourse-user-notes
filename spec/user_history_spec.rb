970b026e21d029a1480bf5fed37d0bd7c92cdb29
Arpit Jalan <arpit@techapj.com>
2017-05-12 22:28:43 +0530

    add specs

---
+++ spec/user_history_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe UserHistory do
+  let(:user) { Fabricate(:user, suspended_till: 7.days.from_now) }
+  let(:admin) { Fabricate(:admin) }
+
+  describe 'when a user suspension log is created' do
+    context "staff notes plugin is enabled" do
+      before do
+        SiteSetting.staff_notes_enabled = true
+      end
+
+      it "should create staff note for suspension" do
+        UserHistory.create!({action: UserHistory.actions[:suspend_user], target_user_id: user.id, acting_user_id: admin.id})
+
+        expect(PluginStore.get('staff_notes', "notes:#{user.id}")).to be_present
+      end
+    end
+  end
+end
---
+++ spec/user_warning_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe UserWarning do
+  let(:user) { Fabricate(:user) }
+  let(:admin) { Fabricate(:admin) }
+  let(:topic) { Fabricate(:topic) }
+
+  describe 'when a user warning is created' do
+    context "staff notes plugin is enabled" do
+      before do
+        SiteSetting.staff_notes_enabled = true
+      end
+
+      it "should create staff note for warning" do
+        UserWarning.create({topic_id: topic.id, user_id: user.id, created_by_id: admin.id})
+
+        expect(PluginStore.get('staff_notes', "notes:#{user.id}")).to be_present
+      end
+    end
+  end
+end
